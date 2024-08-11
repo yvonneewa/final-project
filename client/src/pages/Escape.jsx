29bbb0ee783e4264a986ad487f889c51e7644016
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-08-11 13:39:09 -0600

    added escape

--- client/src/pages/Escape.jsx
+++ client/src/pages/Escape.jsx
@@ -1,34 +1,26 @@
 import React from "react";
-
 const Escape = () => {
-  const handleStartClick = (e) => {
-    e.preventDefault();
-    window.location.href = "/";
-  };
+//   const handleStartClick = (e) => {
+//     e.preventDefault();
+//     window.location.href = "/";
+//   };
 
   return (
-    <div className="home-page">
-    <div className="row mb-4">
-      <div className="col-md-10">
-        <div className="title-container"></div>
-        <div className="fade-in-title">
-          <h1 className="creepster">You have Escaped</h1>
-        </div>
-        <div align="center">
-          <a
-            href="/"
-            id="start-link"
-            className="play-button"
-            onClick={handleStartClick}
-          >
-            Start Over?
-          </a>
+    <div className="escape-page">
+      <div className="credits-container">
+        <div className="credits">
+          <div className="fade-in-title">
+            <h1 className="creepster">You Are Escaped!</h1>
+            <p>Game Design: Yvonne, Shelly</p>
+            <p>Story: TJ</p>
+            <p>Development: Yvonne, Shelly</p>
+            <p>Special Thanks: Joem Casusi</p>
+            <p>Thank you for playing!</p>
+          </div>
         </div>
       </div>
     </div>
-    </div>
   );
 };
-
 
 export default Escape;
